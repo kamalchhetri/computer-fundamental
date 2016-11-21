f6d2070ced89af4274cc6188e499ad1c23a5259c
kamalchhetri <kamalrokkachhetri@gmail.com>
2016-11-21 12:40:03 +0545

    Add files via upload

---
+++ periofrectanglekamal.c
@@ -0,0 +1,23 @@
+/*
+student name:kamal rokka chhetri
+roll no.:
+subject:
+lab no.:
+program:w.a.p to find area and volume of sphere
+date:
+*/
+#include<stdio.h>
+#include<math.h>
+//#include<conio.h>
+int main()
+{
+float l,b,peri;
+printf("enter value l\n");
+scanf("%f",&l);
+printf("enter value b\n");
+scanf("%f",&b);
+peri=l*b;
+
+printf("PERIMETER OF SPHERE=%f\n",peri);
+return 0;
+}
