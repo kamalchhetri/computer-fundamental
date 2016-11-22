d1b818c98c83208bcc13c8088a75e90ae9780f8c
kamalchhetri <kamalrokkachhetri@gmail.com>
2016-11-22 19:55:43 +0545

    Update periofrectanglekamal.c

--- periofrectanglekamal.c
+++ periofrectanglekamal.c
@@ -1,9 +1,9 @@
 /*
 student name:kamal rokka chhetri
 roll no.:
-subject:
+subject:Programming fundamental
 lab no.:
-program:w.a.p to find area and volume of sphere
+program:w.a.p to find area and perimeter of rectangle
 date:
 */
 #include<stdio.h>
@@ -11,13 +11,14 @@ date:
 //#include<conio.h>
 int main()
 {
-float l,b,peri;
+float l,b,peri,area;
 printf("enter value l\n");
 scanf("%f",&l);
 printf("enter value b\n");
 scanf("%f",&b);
-peri=l*b;
-
-printf("PERIMETER OF SPHERE=%f\n",peri);
+peri=2(l+b);
+area=l*b;
+printf("PERIMETER OF RECTANGLE=%f\n",peri);
+printf("AREA OF RECTANGLE=%f\n",area);
 return 0;
 }
