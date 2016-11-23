30eed6bc7665fc87e53678e8c9983f91c2e0c50e
kamalchhetri <kamalrokkachhetri@gmail.com>
2016-11-23 11:11:15 +0545

    Add files via upload

---
+++ quadtricequakamal.c
@@ -0,0 +1,29 @@
+/*
+student name:kamal rokka chhetri
+subject:programming fundamental
+program:w.a.p to find roots of quadratic equation
+roll no.
+date:31,nov,2016
+*/
+#include<stdio.h>
+//#include<conio.h>
+#include<math.h>
+int main()
+{
+float a,b,c,x,y,e;
+printf("enter value of a=\n");
+scanf("%f",&a);
+printf("enter value of b=\n");
+scanf("%f",&b);
+printf("enter value of c=\n");
+scanf("%f",&c);
+e=b*b-4*a*c;
+printf("value of e=%f\n",e);
+x=pow(e,2)/2*a;
+y=-(pow(e,2)/2*a);
+printf("quadratic equation if value is positive=%f\n",x);
+printf("quadratic equation if value is negative=%f",y);
+
+return 0;
+
+}
