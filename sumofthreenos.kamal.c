641e301955ebc2d919a00537c28ccedebd163ffc
kamalchhetri <kamalrokkachhetri@gmail.com>
2016-11-23 10:04:57 +0545

    Add files via upload

---
+++ sumofthreenos.kamal.c
@@ -0,0 +1,23 @@
+/*
+student name:kamal rokka chhetri
+subject:programming fundamental
+program:w.a.p to find su of digits of three nos.
+roll no.
+date:31,nov,2016
+*/
+#include<stdio.h>
+//#include<conio.h>
+int main()
+{
+float a,b,c,sum;
+printf("enter value of a=\n");
+scanf("%f",&a);
+printf("enter value of b=\n");
+scanf("%f",&b);
+printf("enter value of c=\n");
+scanf("%f",&c);
+sum=a+b+c;
+printf("sum of three given nos=%f",sum);
+return 0;
+
+}
