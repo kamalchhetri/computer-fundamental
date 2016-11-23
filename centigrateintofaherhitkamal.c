5102fc4a28bd130cc61c0b2e8799c7d3311e7b87
kamalchhetri <kamalrokkachhetri@gmail.com>
2016-11-23 10:41:04 +0545

    Add files via upload

---
+++ centigrateintofaherhitkamal.c
@@ -0,0 +1,20 @@
+/*
+student name:kamal rokka chhetri
+subject:programming fundamental
+program:w.a.p to find temperature in centigrate into fahernhit.
+roll no.
+date:31,nov,2016
+*/
+#include<stdio.h>
+//#include<conio.h>
+int main()
+{
+float c,f;
+printf("enter value of temperature in fahrenhit\n");
+scanf("%f",&c);
+f=((9*c)+160)/5;
+//c=5/9*(f-32);
+printf("value of temperature of fahrenhit into centigrate =%f",f);
+return 0;
+
+}
