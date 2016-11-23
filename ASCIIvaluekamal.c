2bbab600b592cbe6a6835d397514a80c681d7fee
kamalchhetri <kamalrokkachhetri@gmail.com>
2016-11-23 12:33:26 +0545

    Add files via upload

---
+++ ASCIIvaluekamal.c
@@ -0,0 +1,21 @@
+/*
+student name:kamal rokka chhetri
+subject:programming fundamental
+program:w.a.p to ASCII VALUE OF ANY CHARACTER ASSIGNED BY USER
+roll no.
+date:31,nov,2016
+*/
+#include<stdio.h>
+//#include<conio.h>
+#include<math.h>
+int main()
+{
+char c;
+int asciivalue;
+printf("enter any character");
+scanf("%c",&c);
+asciivalue=c;
+printf("ASCII value of given character:%d",asciivalue);
+return 0;
+
+}
