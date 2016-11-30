cf93104d2d039142797fd3aa1076e38f6948b2e2
kamalchhetri <kamalrokkachhetri@gmail.com>
2016-11-30 12:45:28 +0545

    Add files via upload

---
+++ assignoperatorkamal.c
@@ -0,0 +1,32 @@
+/*
+Student Name :kamal rokka 				
+Roll no.:
+Lab no.:
+Program:assighment operator
+Date:
+subject:
+*/
+			
+				
+			#include<stdio.h>
+			
+				int main()
+				{
+					int a,b;
+					printf("enter first no.:\n");
+					scanf("%d",&a);
+					printf("enter secod no.:\n");
+					scanf("%d",&b);
+					a+=b; //a=a+b
+					printf("result of a+=b::%d\n",a);
+					a-=b;
+					printf("result of a-=b:%d\n",a);
+					a*=b;
+					printf("result of a*=b:%d\n",a);
+					a/=b;
+					
+					printf("result of a/=b:%d\n",a);
+					
+					
+					return 0;
+				}
